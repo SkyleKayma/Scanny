5caea1af324eefe41a8c10716956717946440335
SkyleKayma <lu.godart@gmail.com>
2022-08-20 20:41:36 +0200

    Fix theme and colors declaration

--- app/src/main/java/fr/skyle/scanny/ui/theme/Theme.kt
+++ app/src/main/java/fr/skyle/scanny/ui/theme/Theme.kt
@@ -1,29 +1,12 @@
 package fr.skyle.scanny.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple80,
-    primaryVariant = PurpleGrey80,
-    secondary = Pink80
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple40,
-    primaryVariant = PurpleGrey40,
-    secondary = Pink40
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+import androidx.compose.ui.res.colorResource
+import fr.skyle.scanny.R
 
 @Composable
 fun ScannyTheme(
@@ -31,9 +14,17 @@ fun ScannyTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        darkColors(
+            primary = colorResource(id = R.color.purple_80),
+            primaryVariant = colorResource(id = R.color.purple_grey_80),
+            secondary = colorResource(id = R.color.pink_80)
+        )
     } else {
-        LightColorPalette
+        lightColors(
+            primary = colorResource(id = R.color.purple_40),
+            primaryVariant = colorResource(id = R.color.purple_grey_40),
+            secondary = colorResource(id = R.color.pink_40)
+        )
     }
 
     MaterialTheme(
