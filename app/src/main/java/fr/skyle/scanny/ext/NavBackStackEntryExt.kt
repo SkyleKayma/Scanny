ffec546438d0de4787821fb78f85033377808a4c
Ludovic Godart <l.godart@openium.fr>
2022-08-28 04:06:47 +0200

    Fix Build method target API

--- app/src/main/java/fr/skyle/scanny/ext/NavBackStackEntryExt.kt
+++ app/src/main/java/fr/skyle/scanny/ext/NavBackStackEntryExt.kt
@@ -1,10 +1,12 @@
 package fr.skyle.scanny.ext
 
+import android.annotation.TargetApi
 import android.os.Build
 import android.os.Parcelable
 import androidx.navigation.NavBackStackEntry
 import fr.skyle.scanny.BuildConfig
 
+@TargetApi(Build.VERSION_CODES.BASE)
 fun <T : Parcelable> NavBackStackEntry.getParcelable(key: String, clazz: Class<T>): T? =
     if (BuildConfig.VERSION_CODE >= Build.VERSION_CODES.TIRAMISU) {
         arguments?.getParcelable(key, clazz)
