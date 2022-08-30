d50db6ed3218d87760cf8f50391adea515355c0c
Ludovic Godart <l.godart@openium.fr>
2022-08-30 02:35:14 +0200

    Fix getParcelable issue min API level

--- app/src/main/java/fr/skyle/scanny/ext/NavBackStackEntryExt.kt
+++ app/src/main/java/fr/skyle/scanny/ext/NavBackStackEntryExt.kt
@@ -4,10 +4,9 @@ import android.annotation.TargetApi
 import android.os.Build
 import android.os.Parcelable
 import androidx.navigation.NavBackStackEntry
-import fr.skyle.scanny.BuildConfig
 
 @TargetApi(Build.VERSION_CODES.BASE)
 fun <T : Parcelable> NavBackStackEntry.getParcelable(key: String, clazz: Class<T>): T? =
-    if (BuildConfig.VERSION_CODE >= Build.VERSION_CODES.TIRAMISU) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         arguments?.getParcelable(key, clazz)
     } else arguments?.getParcelable(key)
